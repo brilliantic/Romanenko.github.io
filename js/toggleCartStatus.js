232a5c65168c43ddfa55bf2fe1e5b56e270420b0
brilliantic <125657585+brilliantic@users.noreply.github.com>
2023-04-13 14:13:30 +0300

    Update toggleCartStatus.js

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -1,23 +1,37 @@
-// Відображення статусу кошика Порожня / Повна
-function toggleCartStatus() {
-    // console.log(toggleCartStatus);
+function calcCartPriceAndDelivery() {
+	const cartWrapper = document.querySelector('.cart-wrapper');
+	const priceElements = cartWrapper.querySelectorAll('.price__currency');
+	const totalPriceEl = document.querySelector('.total-price');
+	const deliveryCost = document.querySelector('.delivery-cost');
+	const cartDelivery = document.querySelector('[data-cart-delivery]');
 
-    const cartWrapper = document.querySelector('.cart-wrapper');
-    // console.log(cartWrapper.children.length)
-    const cartEmptyBadge = document.querySelector('[data-cart-empty]');
-    const orderForm = document.querySelector('#order-form');
-    
-    /*  Якщо в кошику більше нуля, то прибираємо повідобрення "кошик пуст"
-        Також, якщо в кошику є товар - додаємо кнопку "замовити" */
-    if (cartWrapper.children.length > 0) {
-        // console.log('FULL');
+	// Загальна вартість товарів
+	let priceTotal = 0;
+	// вартість доставки за замовчуванням
+	const deliveryPrice = 120; 
+	// Мінімальне замовлення для безкоштовної доставки
+	const freeShipping = 600; 
 
-        cartEmptyBadge.style.display = 'none';
-        orderForm.style.display = 'block';
-    } else {
-        // console.log('EMPTY');
-        
-        cartEmptyBadge.style.display = 'block';
-        orderForm.style.display = 'none';
-    }
-};
+	// Обходимо всі блоки з цінами в кошику
+	priceElements.forEach(function (item) {
+		// Знаходимо кількість товарів
+		const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+		// Додаємо вартість товару до загальної вартості (кі-сть * ціну)
+		priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
+	});
+
+	if (priceTotal >= freeShipping) {
+		deliveryPrice = 0; // безкоштовна доставка
+		deliveryCost.classList.add('free');
+		deliveryCost.innerText = 'безкоштовна';
+	} else {
+		deliveryCost.classList.remove('free');
+		deliveryCost.innerText = `${deliveryPrice} грн`;
+	}
+
+	// Додаємо вартість доставки до загальної вартості
+	priceTotal += deliveryPrice;
+
+	// Відображаємо загальну вартість на сторінці
+	totalPriceEl.innerText = priceTotal;
+}
